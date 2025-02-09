8565947f656ff0c5e25eed8a1742d4bf358f7efb
souradip2k4 <22052939@kiit.ac.in>
2025-02-10 02:40:30 +0530

    add: api cache (redis)

---
+++ components/data-table/delete-button.tsx
@@ -0,0 +1,26 @@
+import { Button } from "@/components/ui/button";
+import { QueryEntry } from "@/lib/api";
+import { deleteEntryMutationFn } from "@/lib/query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { Row } from "@tanstack/react-table";
+
+export function DeleteButton({ row }: { row: Row<QueryEntry> }) {
+	const queryClient = useQueryClient();
+	const deleteEntryMutation = useMutation({
+		mutationFn: deleteEntryMutationFn,
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ["entries"] });
+			queryClient.refetchQueries({ queryKey: ["entries"] });
+		},
+	});
+
+	return (
+		<Button
+			size="sm"
+			variant="ghost"
+			className="w-full justify-start"
+			onClick={() => deleteEntryMutation.mutate(row.original.id)}>
+			{deleteEntryMutation.isPending ? "Deleting..." : "Delete"}
+		</Button>
+	);
+}
